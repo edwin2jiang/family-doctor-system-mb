d34b4f3c793b687b11eb65599c134d873e439802
蒋伟伟 <1754082565@qq.com>
2022-04-12 15:15:17 +0800

    change: main.js add mockjs

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import 'vant/lib/index.css';
 
 import { request } from "@/config/Axios.config.js"
 
+if (process.env.NODE_ENV !== 'production') require('./api/index.js') // 添加mockjs
+
 Vue.use(Vant);
 Vue.config.productionTip = false
 Vue.prototype.$axios = request // 将axios挂载到vue的原型上
+
 
 new Vue({
   router,
